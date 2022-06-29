30bac07e0fc32b3c5b396ce808b541fb1d542197
Richard <richard_rodas01@hotmail.com>
2022-06-28 22:47:07 -0500

    Proyecto concluido

--- src/main/java/org/tindertec/controller/SubscripcionController.java
+++ src/main/java/org/tindertec/controller/SubscripcionController.java
@@ -1,16 +1,36 @@
 package org.tindertec.controller;
+
+import org.tindertec.model.Boleta;
 import org.tindertec.model.Usuario;
 import org.tindertec.repository.*;
+
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+
+import java.io.OutputStream;
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 @Controller
 public class SubscripcionController {
-	//@Autowired
-	//@GetMapping
-	//@PostMapping
+	
+	@Autowired
+	private DataSource dataSource; 
+	@Autowired
+	private ResourceLoader resourceLoader; 
+	@Autowired
+	private ISuscripcionRepository repoSub;
+	@Autowired
+	private IBoletaRepository repoBol;
+
 
 	@GetMapping("/Suscripcion")
 	public String cargarMegustas( Model model) {
@@ -18,6 +38,8 @@ public class SubscripcionController {
 		 String nombresYedad =SeguridadController.nombresYedad;
 		 String foto1=SeguridadController.foto1;
 		 int CodUsuInSession=SeguridadController.CodUsuInSession;	 
+		 
+		 model.addAttribute("lstPlanes",repoSub.findAll());
 	//enviarle el usuario que inicio sesion
 		
 		
@@ -26,4 +48,50 @@ public class SubscripcionController {
 
 		return "Suscripcion/Suscripcion";
 		}
+	
+	
+	@PostMapping("/Suscripcion/Boleta1")
+	public void reportes(HttpServletResponse response,Model model) {
+		response.setHeader("Content-Disposition", "inline;"); 
+		response.setContentType("application/pdf");
+		int CodUsuInSession = SeguridadController.CodUsuInSession;
+		
+		try {
+
+			repoBol.USP_Registrar_Boleta(CodUsuInSession, 2, 1);
+			
+			
+			String ru = resourceLoader.getResource("classpath:Reportes/SuscripcionReporte.jasper").getURI().getPath();
+			HashMap parametro = new HashMap<>();
+			parametro.put("codigo", CodUsuInSession);
+			JasperPrint jasperPrint = JasperFillManager.fillReport(ru, parametro, dataSource.getConnection());
+			OutputStream outStream = response.getOutputStream();
+			JasperExportManager.exportReportToPdfStream(jasperPrint, outStream);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	@PostMapping("/Suscripcion/Boleta2")
+	public void reportes2(HttpServletResponse response) {
+		response.setHeader("Content-Disposition", "inline;"); 
+
+		response.setContentType("application/pdf");
+		int CodUsuInSession = SeguridadController.CodUsuInSession;
+		
+		try {
+			repoBol.USP_Registrar_Boleta(CodUsuInSession, 3, 1);
+			String ru = resourceLoader.getResource("classpath:Reportes/SuscripcionReporte.jasper").getURI().getPath();
+			HashMap parametro = new HashMap<>();
+			parametro.put("codigo", CodUsuInSession);
+			JasperPrint jasperPrint = JasperFillManager.fillReport(ru, parametro, dataSource.getConnection());
+			OutputStream outStream = response.getOutputStream();
+			JasperExportManager.exportReportToPdfStream(jasperPrint, outStream);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
--- src/main/java/org/tindertec/repository/IBoletaRepository.java
+++ src/main/java/org/tindertec/repository/IBoletaRepository.java
@@ -1,11 +1,18 @@
 package org.tindertec.repository;
 import org.tindertec.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IBoletaRepository extends JpaRepository <Boleta, Integer>{
 	
-	//Usuario findByCorreoAndClave(String correo,String clave);
-//docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods
+	@Procedure(procedureName = "USP_Registrar_Boleta")
+	void USP_Registrar_Boleta(
+			@Param("cod_usu_sp")int cod_usu,
+			@Param("cod_sub_sp")int cod_sub,
+			@Param("cod_med_pago_sp")int cod_pago
+			);
+
 }
